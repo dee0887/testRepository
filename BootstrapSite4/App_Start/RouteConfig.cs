0baad11c391178d9fa25778bafee56c92687083c
dee0887 <perovd@gmail.com>
2018-10-28 17:39:42 +0300

    format code

--- BootstrapSite4/App_Start/RouteConfig.cs
+++ BootstrapSite4/App_Start/RouteConfig.cs
@@ -12,14 +12,14 @@ namespace BootstrapSite4
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
- routes.MapRoute("Project", "project/{id}", new { controller = "Home", action = "Project" });
+            routes.MapRoute("Project", "project/{id}", new { controller = "Home", action = "Project" });
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
 
-           
+
         }
     }
 }
